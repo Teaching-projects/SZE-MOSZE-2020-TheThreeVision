7760bca9ac3a84907b21f33d18a609bfec0c2fd0
radvanszkyI <45069028+radvanszkyI@users.noreply.github.com>
2020-09-26 19:50:57 +0200

    arg problem solved

--- main.cpp
+++ main.cpp
@@ -9,20 +9,22 @@ int main(int argc, char** argv) {
         throw std::runtime_error("Bad input data.");
     }
     else {
-    Player* Player1 = Player::parseUnit(argv[1]);
-    Player* Player2 = Player::parseUnit(argv[2]);
-    
-    while (Player1->getHP() > 0 && Player2->getHP() > 0)
-    {
-        Player1->Damage(Player2);
-        if (Player2->getHP() > 0) {
-            Player2->Damage(Player1);
+        Player* Player1 = Player::parseUnit(argv[1]);
+        Player* Player2 = Player::parseUnit(argv[2]);
+
+        while (Player1->getHP() > 0 && Player2->getHP() > 0)
+        {
+            Player1->Damage(Player2);
+            if (Player2->getHP() > 0) {
+                Player2->Damage(Player1);
+            }
         }
-    }
-    if (Player1->getHP() == 0) {
-        cout << Player2->getName() << " wins."<< " Remaining HP: "<<Player2->getHP()<<".";
-    }else if(Player2->getHP() == 0){
-        cout << Player1->getName() << " wins."<< " Remaining HP: "<<Player1->getHP()<<".";;
+        if (Player1->getHP() == 0) {
+            cout << Player2->getName() << " wins." << " Remaining HP: " << Player2->getHP() << ".";
+        }
+        else if (Player2->getHP() == 0) {
+            cout << Player1->getName() << " wins." << " Remaining HP: " << Player1->getHP() << ".";;
+        }
     }
 
     return 0;
--- player.cpp
+++ player.cpp
@@ -7,46 +7,48 @@
 
 void Player::Damage(Player* player)
 {
-	
-	if ((player->HP - DMG) >= 0)
-	{
-		player->HP -= DMG;
 
-	}
+    if ((player->HP - DMG) >= 0)
+    {
+        player->HP -= DMG;
+
+    }
     else { player->HP = 0; }
-	
+
 }
 
-Player* Player::parseUnit(const std::string filename){
+Player* Player::parseUnit(const std::string filename) {
     std::ifstream file(filename);
     std::vector<std::string> data;
     std::string newLine, name;
-    int hp,dmg;
+    int hp, dmg;
     if (file.good()) {
         while (getline(file, newLine)) {
             data.push_back(newLine);
         }
         data[0].erase();
-        data[data.size()-1].erase();
+        data[data.size() - 1].erase();
         for (int i = 0; i < data.size(); i++) {
             int c;
             c = data[i].find('"');
-            if (data[i][c+1] == 'n') {
+            if (data[i][c + 1] == 'n') {
                 c = data[i].find(':');
                 std::string temp;
-                temp = data[i].substr(c+3,data[i].length());
-                name = temp.substr(0,temp.length()-2);
-            }else if (data[i][c+1] == 'h'){
+                temp = data[i].substr(c + 3, data[i].length());
+                name = temp.substr(0, temp.length() - 2);
+            }
+            else if (data[i][c + 1] == 'h') {
                 c = data[i].find(':');
                 std::string temp;
-                temp = data[i].substr(c+2,data[i].length());
-                temp = temp.substr(0,temp.length()-1);
+                temp = data[i].substr(c + 2, data[i].length());
+                temp = temp.substr(0, temp.length() - 1);
                 hp = atoi(temp.c_str());
-            } else if(data[i][c+1] == 'd'){
+            }
+            else if (data[i][c + 1] == 'd') {
                 c = data[i].find(':');
                 std::string temp;
-                temp = data[i].substr(c+2,data[i].length());
-                temp = temp.substr(0,temp.length());
+                temp = data[i].substr(c + 2, data[i].length());
+                temp = temp.substr(0, temp.length());
                 dmg = atoi(temp.c_str());
             }
         }
@@ -54,11 +56,11 @@ Player* Player::parseUnit(const std::string filename){
     else {
         throw std::runtime_error("There is a problem with the file: " + filename);
     }
-    return new Player(name,hp,dmg);
+    return new Player(name, hp, dmg);
 }
 
-std::ostream & operator<<(std::ostream & o, Player & d)
+std::ostream& operator<<(std::ostream& o, Player& d)
 {
-	return o << d.getName() << ": HP: " << d.getHP() << ", DMG: " << d.getDMG();
+    return o << d.getName() << ": HP: " << d.getHP() << ", DMG: " << d.getDMG();
 }
 
