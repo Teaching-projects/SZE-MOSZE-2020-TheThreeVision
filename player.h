b6059dc832cd27f91b0a9b4ca13e3d967be5f36d
borbelytomii <55625187+borbelytomii@users.noreply.github.com>
2020-10-11 17:27:57 +0200

    Levelup implementing

--- player.h
+++ player.h
@@ -1,19 +1,39 @@
 #pragma once
 #include <iostream>
-
+#include <string>
 class Player
 {
 	const std::string Name;
 	int HP;
-	const int DMG;
+	int DMG;
+protected:
+	void Damage(Player* player)const;
 public:
 	Player(std::string name, int hp, int dmg) :Name(name), HP(hp), DMG(dmg) {}
-
+	void setHP(int h) { HP = h; }
 	std::string getName() const { return Name; }
-	int	getHP() const { return HP; }
-	int getDMG() const { return DMG; }
+	int	getHP() { return HP; }
+	void setDMG(int d) { DMG = d; }
+	int  getDMG()const { return DMG; }
 
 	friend std::ostream& operator<<(std::ostream& o, Player& d);
-	void Damage(Player* player);
+	void Fight(Player* player);
 	static Player* parseUnit(const std::string file);
+
+
+};
+
+class AdventurerPlayer : public Player
+{
+private:
+	int XP=0;
+	int maxHP;
+
+public:
+	void levelup(int levelnew);
+	void Fight(AdventurerPlayer* otherplayer);
+	AdventurerPlayer(std::string name, int hp, int dmg) :Player(name, hp, dmg), maxHP(hp){};
+	void increaseXP(int dmg);
+	int getXP() { return XP; }
+	static AdventurerPlayer* parseUnit(const std::string file);
 };
