81257274bbb36e810bff5aa49505fa792a6a2f28
radvanszkyI <45069028+radvanszkyI@users.noreply.github.com>
2020-10-14 11:30:09 +0200

    doxygen

--- player.h
+++ player.h
@@ -1,28 +1,54 @@
 #pragma once
 #include <iostream>
 /**
-* \class Player
-* \brief Player class
-*/
+ * \class Player
+ * 
+ * \brief Player class
+ * 
+ * \author radvanszkyI
+ *
+ * \date 2020.10.13. 22:22
+ *
+ * \note A player can fight with an other player while one of them is die.
+ * The fight end is depend on the two player's HealPoints, Damages, and the AttackCooldowns.
+ */
 class Player
 {
-	const std::string Name;
-	int HP;
-	const int DMG;
-	double AtkCoolDown;
+private:
+	const std::string Name; ///< the Name of the player
+	int HP;					///< the HealPoints of the player
+	const int DMG;			///< the Damage of the player
+	double AtkCoolDown;		///< the player's Attack Cooldown shows that after how many seconds he/she can hit again. 
 
-	void Damage (Player* player)const;
+	/// this method damage the other player
+	void Damage (Player* player/**[in] the player who is injured*/)const;
 public:
+	
+	/// player construct, what put the input parameters to the data members
 	Player(std::string name, int hp, int dmg, double atkCoolDown) :Name(name), HP(hp), DMG(dmg), AtkCoolDown(atkCoolDown) {}
 
-
+	///simple getter to the Name
 	std::string getName() const { return Name; }
+	///simple getter to the HealPoint
 	int	getHP() const { return HP; }
+	///simple getter to the Damage
 	int getDMG() const { return DMG; }
+	///simple getter to the AttackCooldown
 	double getAtkCoolDown() const { return AtkCoolDown; }
+	/// simple setter to the AttackCooldown
 	void setAtkCoolDown(int atkCoolDown)  {  AtkCoolDown=  atkCoolDown; }
 
-	friend std::ostream& operator<<(std::ostream& o, Player& d);
-	void Fight(Player* player);
-	static Player* parseUnit(const std::string file);
+	
+	/// easy way to print an player
+	friend std::ostream& operator<<(std::ostream& o/**[in] is the output stream*/, Player& d/**[in] is the printed player*/);
+	/**
+	 * \brief fight with another player while one of them is die
+	 */
+	void Fight(Player* player/**[in] is the other player*/);
+	/**
+	 * \brief This method reads a .json file and get the useful informations.
+	 * \return return with a Player object
+	 * \throw throw a "runtime_error" if something wrong
+	 */
+	static Player* parseUnit(const std::string file/**[in] is the .json file's name*/);
 };
