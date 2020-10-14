378b25c3a8e2130fece295c9ea69d802baca4b87
poloczmate <48856366+poloczmate@users.noreply.github.com>
2020-10-14 16:57:53 +0200

    added googletest

---
+++ test/unit_test.cpp
@@ -0,0 +1,52 @@
+#include <gtest/gtest.h>
+#include <string>
+#include <map>
+#include <fstream>
+#include "parser.h"
+
+TEST(ParserTest, TestingWithFileInput)
+{
+	std::string inputFilename = "unit_test.json";
+	std::map<std::string, std::string> expected;
+	expected.insert(std::pair<std::string, std::string>("name", "vizibicikli"));
+	expected.insert(std::pair<std::string, std::string>("hp", "15330"));
+	expected.insert(std::pair<std::string, std::string>("dmg", "1450"));
+
+    std::ifstream inputFile(inputFilename);
+
+	std::map<std::string, std::string> testingFunction = JsonParser::parseJson(inputFile);
+
+    inputFile.close();
+
+	ASSERT_EQ(expected, testingFunction);
+}
+
+TEST(ParserTest, TestingWithFilenameInput)
+{
+	std::string inputFilename = "unit_test.json";
+	std::map<std::string, std::string> expected;
+	expected.insert(std::pair<std::string, std::string>("name", "vizibicikli"));
+	expected.insert(std::pair<std::string, std::string>("hp", "15330"));
+	expected.insert(std::pair<std::string, std::string>("dmg", "1450"));
+	std::map<std::string, std::string> testingFunction = JsonParser::parseJsonFilename(inputFilename);
+
+	ASSERT_EQ(expected, testingFunction);
+}
+
+TEST(ParserTest, TestingWithStringInput)
+{
+	std::string inputString = "{\n\t\"name\"\t   :  \"vizibicikli\",\n\t\"hp\":15330,\n\t\"dmg\"   :    1450\n}";
+	std::map<std::string, std::string> expected;
+	expected.insert(std::pair<std::string, std::string>("name", "vizibicikli"));
+	expected.insert(std::pair<std::string, std::string>("hp", "15330"));
+	expected.insert(std::pair<std::string, std::string>("dmg", "1450"));
+	std::map<std::string, std::string> testingFunction = JsonParser::parseJsonString(inputString);
+
+	ASSERT_EQ(expected, testingFunction);
+}
+
+int main(int argc, char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
