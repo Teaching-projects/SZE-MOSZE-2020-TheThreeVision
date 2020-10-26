e331a6260ed9bad082cbb2002967b3970c745ee1
borbelytomii <55625187+borbelytomii@users.noreply.github.com>
2020-10-26 15:14:21 +0100

    added unittest

--- test/unit_test.cpp
+++ test/unit_test.cpp
@@ -35,6 +35,59 @@ TEST(ParserTest, TestingWithStringInput)
 
 	ASSERT_THROW(Parser::ParseJsonString(inputString), std::runtime_error);
 }
+//new branch tesztek innentol
+
+TEST(ParserTest2, TestingWithFileInput_randomkeys)
+{
+	std::string inputFilename = "test/unit2.json";
+	std::map<std::string, std::string> expected;
+	expected.insert(std::pair<std::string, std::string>("name", "vizibicikli"));
+	expected.insert(std::pair<std::string, std::string>("hp", "15330"));
+	expected.insert(std::pair<std::string, std::string>("dmg", "1450"));
+	expected.insert(std::pair<std::string, std::string>("atkcd", "2"));
+
+	std::ifstream inputFile(inputFilename);
+
+	std::map<std::string, std::string> testingFunction = Parser::ParseJson(inputFile);
+
+	inputFile.close();
+
+	ASSERT_EQ(expected, testingFunction);
+
+}
+TEST(ParserTest2, TestingWithFilenameInput_randomkeys)
+{
+	std::string inputFilename = "test/unit2.json";
+	std::map<std::string, std::string> expected;
+	expected.insert(std::pair<std::string, std::string>("name", "vizibicikli"));
+	expected.insert(std::pair<std::string, std::string>("hp", "15330"));
+	expected.insert(std::pair<std::string, std::string>("dmg", "1450"));
+	expected.insert(std::pair<std::string, std::string>("atkcd", "2"));
+
+	
+
+	std::map<std::string, std::string> testingFunction = Parser::ParseJsonFilename(inputFilename);
+
+	ASSERT_EQ(expected, testingFunction);
+
+	
+}
+TEST(ParserTest, TestingWithStringInput)
+{
+	std::string inputString = "{\n\t\"name\"  :  \"vizibicikli\",\n\t\"dmg\":1450,\n\t\"hp\"   :    15330,\n \"atkcd\": 2}";
+
+	std::map<std::string, std::string> expected;
+	expected.insert(std::pair<std::string, std::string>("name", "vizibicikli"));
+	expected.insert(std::pair<std::string, std::string>("hp", "15330"));
+	expected.insert(std::pair<std::string, std::string>("dmg", "1450"));
+	expected.insert(std::pair<std::string, std::string>("atkcd", "2"));
+
+
+
+	std::map<std::string, std::string> testingFunction = Parser::ParseJsonString(inputFilename);
+
+	ASSERT_EQ(expected, testingFunction);
+}
 
 int main(int argc, char** argv)
 {
