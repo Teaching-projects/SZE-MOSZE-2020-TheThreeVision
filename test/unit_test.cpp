244df9020493736a16341c46ac1d42a5e35608c1
borbelytomii <55625187+borbelytomii@users.noreply.github.com>
2020-11-01 16:37:56 +0100

    changed order

--- test/unit_test.cpp
+++ test/unit_test.cpp
@@ -90,40 +90,35 @@ TEST(ParserTest2, TestingWithStringInput_randomkeys)
 }
 TEST(Finddatatests, ALL_DATA_TESTS)
 {
-
-	std::string toFind = "hp";
-	std::string expected;
-
 	std::string inputString = "{\n\t\"name\":\"vizibicikli\",\n\t\"dmg\":1450,\n\t\"hp\":15330,\n\"atkcd\":2}";
-	expected = "15330";
-	std::string data = Parser::FindData(inputString, toFind);
+	std::string toFind = "hp";
+	std::string expected = "15330";;
 
+
+	std::string data = Parser::FindData(inputString, toFind);
 	ASSERT_EQ(expected, data);
 
 
 	std::string toFind1 = "name";
-	std::string expected1;
+	std::string expected1 = "vizibicikli";;
 
 
-	expected1 = "vizibicikli";
 	std::string data1 = Parser::FindData(inputString, toFind1);
-
 	ASSERT_EQ(expected1, data1);
 
-	std::string toFind2 = "dmg";
-	std::string expected2;
 
-	
-	expected2 = "1450";
+	std::string toFind2 = "dmg";
+	std::string expected2 = "1450";
+
+
 	std::string data2 = Parser::FindData(inputString, toFind2);
 
 	ASSERT_EQ(expected2, data2);
 
 	std::string toFind3 = "atkcd";
-	std::string expected3;
+	std::string expected3 = "2";;
 
-	
-	expected3 = "2";
+
 	std::string data3 = Parser::FindData(inputString, toFind3);
 
 	ASSERT_EQ(expected3, data3);
@@ -133,16 +128,16 @@ TEST(Finddatatests, ALL_DATA_TESTS)
 
 TEST(Finddatatest, TestingWithfalsedata)
 {
-	std::string toFind = "atcd";
-
 	std::string inputString = "{\n\t\"name\":\"vizibicikli\",\n\t\"dmg\":1450,\n\t\"hp\":15330,\n\"atkcd\":2}";
+	std::string toFind = "atcd";
+	
 
 	ASSERT_THROW(Parser::FindData(inputString, toFind), std::runtime_error);
 }
 
 TEST(ParserTest2, TestingWithFilenameInput_good)
 {
-	std::string inputFilename = "test/unit2.json";
+	std::string inputFilename = "test/unit.json";
 	std::map<std::string, std::string> expected;
 	expected.insert(std::pair<std::string, std::string>("name", "vizibicikli"));
 	expected.insert(std::pair<std::string, std::string>("hp", "15330"));
@@ -180,12 +175,11 @@ TEST(ParserTest2, TestingWithFilenameInput)
 
 TEST(Finddatatest, Nametest_withspace)
 {
-
+std::string inputString = "{\n\t\"name\":\"vizi bicikli\",\n\t\"dmg\":1450,\n\t\"hp\":15330,\n\"atkcd\":2}";
 	std::string toFind = "name";
-	std::string expected;
-
-	std::string inputString = "{\n\t\"name\":\"vizi bicikli\",\n\t\"dmg\":1450,\n\t\"hp\":15330,\n\"atkcd\":2}";
-	expected = "vizi bicikli";
+	std::string expected= "vizi bicikli";;
+	
+	
 	std::string data = Parser::FindData(inputString, toFind);
 
 	ASSERT_EQ(expected, data);
