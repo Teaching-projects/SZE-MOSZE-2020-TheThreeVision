a7c3967ab94b4b248b04fb2e364ff65fd0accf29
poloczmate <48856366+poloczmate@users.noreply.github.com>
2020-11-02 12:08:55 +0000

    Update gh-pages to output generated at aa41368

--- search/all_2.js
+++ search/all_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['fight',['Fight',['../classPlayer.html#a84ea8d63b619770a3eb3733c44dccd11',1,'Player::Fight()'],['../classAdventurerPlayer.html#aec9e995cabf2d0e47775d0e33be58f95',1,'AdventurerPlayer::Fight()']]]
+  ['fight',['Fight',['../classPlayer.html#a84ea8d63b619770a3eb3733c44dccd11',1,'Player::Fight()'],['../classAdventurerPlayer.html#aec9e995cabf2d0e47775d0e33be58f95',1,'AdventurerPlayer::Fight()']]],
+  ['finddata',['FindData',['../classParser.html#a4c9b872ea43cd479a3bd1a1037f34538',1,'Parser']]]
 ];
