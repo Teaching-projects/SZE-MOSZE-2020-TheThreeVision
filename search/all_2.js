55c10bcb5079f2f577ce969d6323e6615b282640
borbelytomii <55625187+borbelytomii@users.noreply.github.com>
2020-11-04 16:37:34 +0000

    Update gh-pages to output generated at f07774d

--- search/all_2.js
+++ search/all_2.js
@@ -1,9 +1,10 @@
 var searchData=
 [
-  ['getatkcooldown',['getAtkCoolDown',['../classMonster.html#aa7c9b2b5f6d2eb6e08d1eedb3198dea4',1,'Monster']]],
-  ['getdmg',['getDMG',['../classMonster.html#a847ae7d231832102f69a6f9e1a5bac9a',1,'Monster']]],
-  ['gethp',['getHP',['../classMonster.html#ae64382925185bd876e4b8d1d636e0a32',1,'Monster']]],
+  ['getattackcooldown',['getAttackCoolDown',['../classMonster.html#ad84dbe06c7a2aa7f75164dd9db52daf3',1,'Monster']]],
+  ['getdamage',['getDamage',['../classMonster.html#a5872e19f684e0a81ead15da3e82992ad',1,'Monster']]],
+  ['gethealthpoints',['getHealthPoints',['../classMonster.html#acbfb552014439fe02d10f2492e60fe34',1,'Monster']]],
   ['getlevel',['getLevel',['../classHero.html#a5e57cbaa03afdb8ee9b0c5c3505a76be',1,'Hero']]],
+  ['getmaxhealthpoints',['getMaxHealthPoints',['../classHero.html#a102ed7a15cb4a5f7058e33a435518f06',1,'Hero']]],
   ['getname',['getName',['../classMonster.html#a28ae1112e37262ce150e3c02465b3dd5',1,'Monster']]],
   ['getxp',['getXP',['../classHero.html#af8f4b26fcf84d976a492ce974ad108f1',1,'Hero']]]
 ];
