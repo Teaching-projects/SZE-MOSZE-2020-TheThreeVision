8eee2a813799ad491b673a1a4250accb06fb011c
radvanszkyI <45069028+radvanszkyI@users.noreply.github.com>
2020-11-04 16:18:43 +0000

    Update gh-pages to output generated at bc02a2d

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['damage',['Damage',['../classHero.html#ad5b71c939211d80ddaa887167a04d235',1,'Hero::Damage()'],['../classMonster.html#a6301d5ddb5f3387f57874dd517bd2211',1,'Monster::Damage()']]]
+  ['damage',['Damage',['../classHero.html#ad5b71c939211d80ddaa887167a04d235',1,'Hero::Damage(Hero *player)'],['../classHero.html#a2f279b816d70b415aaf0d3b83ae50223',1,'Hero::Damage(Monster *monster)'],['../classMonster.html#a5d338ba042f4addf730ee7764aa7c0ee',1,'Monster::Damage()']]]
 ];
--- search/functions_2.js
+++ search/functions_2.js
@@ -3,6 +3,7 @@ var searchData=
   ['getatkcooldown',['getAtkCoolDown',['../classMonster.html#aa7c9b2b5f6d2eb6e08d1eedb3198dea4',1,'Monster']]],
   ['getdmg',['getDMG',['../classMonster.html#a847ae7d231832102f69a6f9e1a5bac9a',1,'Monster']]],
   ['gethp',['getHP',['../classMonster.html#ae64382925185bd876e4b8d1d636e0a32',1,'Monster']]],
+  ['getlevel',['getLevel',['../classHero.html#a5e57cbaa03afdb8ee9b0c5c3505a76be',1,'Hero']]],
   ['getname',['getName',['../classMonster.html#a28ae1112e37262ce150e3c02465b3dd5',1,'Monster']]],
   ['getxp',['getXP',['../classHero.html#af8f4b26fcf84d976a492ce974ad108f1',1,'Hero']]]
 ];
