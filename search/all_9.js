0b8ee021db33390354f3dd6fafadf1e93b79a95b
poloczmate <48856366+poloczmate@users.noreply.github.com>
2020-11-06 00:49:18 +0000

    Update gh-pages to output generated at 2a0770c

--- search/all_9.js
+++ search/all_9.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['parse',['parse',['../classHero.html#acdd608cda56d150ac7ff5a4c29e0cc42',1,'Hero::parse()'],['../classMonster.html#abe91cd8127c815d37f3b3b4577a51b72',1,'Monster::parse()']]],
   ['parseexception',['ParseException',['../classJSON_1_1ParseException.html',1,'JSON']]],
-  ['parsejsonfilename',['ParseJsonFilename',['../classJSON.html#a5ccacfb112e05f3eea36f431353460d1',1,'JSON']]]
+  ['parsejsonfilename',['ParseJsonFilename',['../classJSON.html#a5ccacfb112e05f3eea36f431353460d1',1,'JSON']]],
+  ['parsejsonstring',['ParseJsonString',['../classJSON.html#ae8a7248f0e50187bae71c34ea6621ef6',1,'JSON']]]
 ];
