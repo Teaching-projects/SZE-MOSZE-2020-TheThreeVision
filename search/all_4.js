54bbd5f55998e3c93bfc684177ca79784f920236
poloczmate <48856366+poloczmate@users.noreply.github.com>
2020-11-30 16:20:58 +0000

    Update gh-pages to output generated at f29ea7e

--- search/all_4.js
+++ search/all_4.js
@@ -6,7 +6,7 @@ var searchData=
   ['getdamage_9',['getDamage',['../classMonster.html#a56b198cee3796d9e311a7840266c9c6c',1,'Monster']]],
   ['getdefense_10',['getDefense',['../classMonster.html#aafce7d1ded2630ea0d1d1089858b8fc0',1,'Monster']]],
   ['gethealthpoints_11',['getHealthPoints',['../classMonster.html#acbfb552014439fe02d10f2492e60fe34',1,'Monster']]],
-  ['getheight_12',['getHeight',['../classMap.html#a2b09c8875af2efb711fc3a022e70427d',1,'Map']]],
+  ['getheight_12',['getHeight',['../classMap.html#a67113f1ffac80d9a20b15fa18a58edc2',1,'Map']]],
   ['getlevel_13',['getLevel',['../classHero.html#a5e57cbaa03afdb8ee9b0c5c3505a76be',1,'Hero']]],
   ['getmaxhealthpoints_14',['getMaxHealthPoints',['../classHero.html#a102ed7a15cb4a5f7058e33a435518f06',1,'Hero']]],
   ['getname_15',['getName',['../classMonster.html#a28ae1112e37262ce150e3c02465b3dd5',1,'Monster']]],
