2ca3f54d927b7c9307f11f9a7a1cc00e3ec517d4
poloczmate <48856366+poloczmate@users.noreply.github.com>
2020-12-07 10:13:14 +0100

    getters to public

--- Hero.h
+++ Hero.h
@@ -30,29 +30,16 @@ private:
 	double Cooldown_multiplier_per_level;
 	double Light_radius;
 	double Light_radius_bonus_per_level;
-	/// a simple getter to the Experience per level
-	double getExperience_per_level() { return Experience_per_level; }
-	/// a simple getter to the Health point bonus per level
-	double	getHealth_point_bonus_per_level() { return Health_point_bonus_per_level; }
-	/// a simple getter to the Damage bonus per level
-	damage	getDamage_bonus_per_level() { return Damage_bonus_per_level; }
-	/// a simple getter to the Defense bonus per level
-	double	getDefense_bonus_per_level() { return Defense_bonus_per_level; }
-	/// a simple getter to the Cooldown multiplier per level
-	double	getCooldown_multiplier_per_level() { return Cooldown_multiplier_per_level; }
 	/**
 	 * \brief This method damages a monster.
 	 * \note this method does the increaseXP and the levelup
 	 */
 	void Damage(Monster* monster/**[in] the monster whitch is injured*/);
 protected:
-
-	
 	/// this method increases maxHP, DMG and decreases AtkCoolDown and sets the HP to maximum
 	void levelup(int levels/**[in] is the number of the level increases*/);
 	/// add more XP to XP
 	void increaseXP(damage dmg/**[in] contains the physical and the magical damage whitch give the added XP amount */);
-
 public:
 	/**
 	 * \brief fight with a monster while one of them is die
@@ -84,6 +71,16 @@ public:
 	int getMaxHealthPoints() { return maxHP; }
 	/// a simple getter to the lightradius
 	int getLightRadius(){ return int(Light_radius);}
+	/// a simple getter to the Experience per level
+	double getExperience_per_level() { return Experience_per_level; }
+	/// a simple getter to the Health point bonus per level
+	double	getHealth_point_bonus_per_level() { return Health_point_bonus_per_level; }
+	/// a simple getter to the Damage bonus per level
+	damage	getDamage_bonus_per_level() { return Damage_bonus_per_level; }
+	/// a simple getter to the Defense bonus per level
+	double	getDefense_bonus_per_level() { return Defense_bonus_per_level; }
+	/// a simple getter to the Cooldown multiplier per level
+	double	getCooldown_multiplier_per_level() { return Cooldown_multiplier_per_level; }
 	/**
 	 * \brief This method use the parser class, and reads a .json file, and get the keys and the attached data and returns a new AdventurerPlayer object
 	 * \return return with an Hero object
